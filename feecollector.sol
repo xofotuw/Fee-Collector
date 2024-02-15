1684f281ed8a4d3e0225d42b676a253a25792719
xofotuw <50031943+xofotuw@users.noreply.github.com>
2024-02-15 15:35:37 +0800

    Create feecollector.sol

---
+++ feecollector.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.7;
+
+contract FeeColelctor {
+	address public owner;
+	
+	constructor(){
+		owner = msg.sender;
+	}
+	receive() payable external (
+	balance += msg.value;
+	}
+	
+	function withdraw(uint amount, address payable destAddr) public {
+	require(msg.sender == owner, "Only owner can withdraw");
+	require(amount <= balance, "Insufficient funds);
+	
+	destAddr.transfer(amount);
+	balance -= amount;
+}
